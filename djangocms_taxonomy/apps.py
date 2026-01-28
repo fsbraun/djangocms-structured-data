a068a0e03c962885c28f8d4ce8ae6f303de164ab
Fabian Braun <fsbraun@gmx.de>
2026-01-28 23:34:38 +0100

    chore: Update test-matrix

--- djangocms_taxonomy/apps.py
+++ djangocms_taxonomy/apps.py
@@ -1,7 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class DjangoTaxonomyConfig(AppConfig):
     name = "djangocms_taxonomy"
-    verbose_name = "Django CMS Taxonomy"
+    verbose_name = _("Taxonomy")
     default_auto_field = "django.db.models.BigAutoField"
--- djangocms_taxonomy/cms_config.py
+++ djangocms_taxonomy/cms_config.py
@@ -3,5 +3,4 @@ from cms.app_base import CMSAppConfig
 
 class DjangoTaxonomyCMSConfig(CMSAppConfig):
     cms_enabled = True
-    cms_toolbar_enabled_admin = False
-    cms_config_enabled = True
+  
